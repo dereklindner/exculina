0fe5a19064047652de8a9f10c097545265965ce6
Derek Lindner <derek@alum.mit.edu>
2013-04-12 00:50:44 -0700

    bruncherator

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 var http = require("http");
 
+var b1 = ["oven-baked", "house-cured", "butter-basted", "creamy", "crispy", "hot", "smoked", "slow-cooked", "sweet", "savory", "Ivo's", "skillet-fried", "organic", "artisan"];
+var b2 = ["ham", "egg", "salmon", "strawberry", "lemon", "blueberry", "banana", "apple", "vanilla", "rosemary", "nutella", "buttermilk", "cinnamon", "molasses", "brown sugar", "pecan", "bourbon"];
+var b3 = ["cheddar", "cream cheese", "mascarpone", "ricotta", "champagne", "potato", "yogurt", "bacon", "sausage", "shrimp", "pork belly", "trout", "poppy seed", "maple syrup"];
+var b4 = ["omelette", "bagel", "biscuit", "waffles", "pancakes", "croque monsieur", "beignets", "dutch baby", "strata", "fritatta", "quiche", "croissant", "benedict", "scone", "muffin", "brioche", "scramble", "hash", "toast", "french toast", "grits", "salad", "tartine", "sticky buns", "chilaquiles", "spoonbread", "oatmeal"];
+
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello, World");
+  var s = b1[Math.floor(Math.random()*b1.length)] + " " + b2[Math.floor(Math.random()*b2.length)] + 
+     " and " + b3[Math.floor(Math.random()*b3.length)] + " " + b4[Math.floor(Math.random()*b4.length)];
+
+  response.write(s);
   response.end();
 }).listen(process.env.PORT || 8888);
+
+console.log("up!");
