900d8da78426e96a39625c88986a0ca1503d764d
Derek Lindner <derek@alum.mit.edu>
2013-04-14 09:43:04 -0700

    spelling: frittata

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require("http");
 var b1 = ["Oven-baked", "House-cured", "Butter-basted", "Creamy", "Crispy", "Hot", "Smoked", "Slow-cooked", "Sweet", "Savory", "Ivo's", "Skillet-fried", "Organic", "Artisan"];
 var b2 = ["ham", "egg", "salmon", "strawberry", "lemon", "blueberry", "banana", "apple", "vanilla", "rosemary", "nutella", "buttermilk", "cinnamon", "molasses", "brown sugar", "pecan", "bourbon"];
 var b3 = ["cheddar", "cream cheese", "mascarpone", "ricotta", "champagne", "potato", "yogurt", "bacon", "sausage", "shrimp", "pork belly", "trout", "poppy seed", "maple syrup"];
-var b4 = ["omelette", "bagel", "biscuits", "waffles", "pancakes", "croque monsieur", "beignets", "dutch baby", "strata", "fritatta", "quiche", "croissant", "benedict", "scone", "muffin", "brioche", "scramble", "hash", "toast", "french toast", "grits", "salad", "tartine", "sticky buns", "chilaquiles", "spoonbread", "oatmeal"];
+var b4 = ["omelette", "bagel", "biscuits", "waffles", "pancakes", "croque monsieur", "beignets", "dutch baby", "strata", "frittata", "quiche", "croissant", "benedict", "scone", "muffin", "brioche", "scramble", "hash", "toast", "french toast", "grits", "salad", "tartine", "sticky buns", "chilaquiles", "spoonbread", "oatmeal"];
 
 var brunch = function (request,response) {
     return b1[Math.floor(Math.random()*b1.length)] + " " + b2[Math.floor(Math.random()*b2.length)] + 
